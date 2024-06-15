962422583e83cd55af195cfac0f8f8ce7499e4ab
Sdg877 <sdrakegill@gmail.com>
2024-06-15 11:14:40 +0100

    removing unused code

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -13,17 +13,4 @@ export function checkToken(){
   return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/check-token`);
 }
 
-// import API_BASE_URL from './config';
-// import sendRequest from './send-request';
-
-// export function signUp(userData) {
-//   return sendRequest(`${API_BASE_URL}/api/users`, 'POST', userData);
-// }
-
-// export function login(credentials) {
-//   return sendRequest(`${API_BASE_URL}/api/users/login`, 'POST', credentials);
-// }
-
-// export function checkToken() {
-//   return sendRequest(`${API_BASE_URL}/check-token`);
 

58c97e829d361d5ffae580ed72760a94c3141bce
Sdg877 <sdrakegill@gmail.com>
2024-06-15 11:00:09 +0100

    updating backend urls

---
+++ config.js
@@ -0,0 +1,4 @@
+
+const API_BASE_URL = 'https://gourmetgallerybackend.onrender.com'; 
+
+export default API_BASE_URL;
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,15 +1,29 @@
+// import sendRequest from './send-request';
+
+// export function signUp(userData) {
+//   return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/api/users`, 'POST', userData);
+// }
+
+
+// export function login(credentials) {
+//   return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/api/users/login`, 'POST', credentials);
+// }
+
+// export function checkToken(){
+//   return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/check-token`);
+// }
+
+import API_BASE_URL from './config';
 import sendRequest from './send-request';
 
 export function signUp(userData) {
-  return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/api/users`, 'POST', userData);
+  return sendRequest(`${API_BASE_URL}/api/users`, 'POST', userData);
 }
-
 
 export function login(credentials) {
-  return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/api/users/login`, 'POST', credentials);
+  return sendRequest(`${API_BASE_URL}/api/users/login`, 'POST', credentials);
 }
 
-export function checkToken(){
-  return sendRequest(`${process.env.REACT_APP_BACKEND_URL}/check-token`);
+export function checkToken() {
+  return sendRequest(`${API_BASE_URL}/check-token`);
 }
-

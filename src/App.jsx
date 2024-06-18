e2e021a53f89cb7bd18b2b1e5b851ffc0ea2751c
Sdg877 <sdrakegill@gmail.com>
2024-06-18 13:56:34 +0100

    further trouble shotting navbar issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+// import React, { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import LoginPage from './pages/LoginPage.jsx';
+// import ProfilePage from './pages/ProfilePage.jsx';
+// import HomePage from '../src/pages/HomePage.jsx';
+// import NavBar from './components/NavBar.jsx';
+// import RecipeView from './pages/RecipeView.jsx';
+
+// function App() {
+//   const [user, setUser] = useState(null);
+
+//   return (
+//     <div className='App'>
+//       <NavBar isLoggedIn={!!user} />
+//       <Routes>
+//         <Route path="/login" element={<LoginPage setUser={setUser} />} />
+//         <Route path="/profile" element={<ProfilePage />} />
+//         <Route path="/recipes/:id" element={<RecipeView />} />
+//         <Route path="/" element={<HomePage />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -6,15 +33,29 @@ import ProfilePage from './pages/ProfilePage.jsx';
 import HomePage from '../src/pages/HomePage.jsx';
 import NavBar from './components/NavBar.jsx';
 import RecipeView from './pages/RecipeView.jsx';
+import { getUser } from '../utilities/users-service'; // import getUser
 
 function App() {
-  const [user, setUser] = useState(null);
+  // Manage authentication state
+  const [isLoggedIn, setIsLoggedIn] = useState(!!getUser());
+
+  // Function to handle login
+  const handleLogin = () => {
+    setIsLoggedIn(true); // Update isLoggedIn state
+  };
+
+  // Function to handle logout
+  const handleLogout = () => {
+    setIsLoggedIn(false); // Update isLoggedIn state
+  };
 
   return (
     <div className='App'>
-      <NavBar isLoggedIn={!!user} />
+      {/* Pass isLoggedIn and handleLogout to NavBar */}
+      <NavBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
-        <Route path="/login" element={<LoginPage setUser={setUser} />} />
+        {/* Pass handleLogin to LoginPage */}
+        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/recipes/:id" element={<RecipeView />} />
         <Route path="/" element={<HomePage />} />
@@ -24,5 +65,4 @@ function App() {
 }
 
 export default App;
-
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,21 +22,44 @@
 // }
 
 
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import '../App.css'; 
+// import { getUser } from '../utilities/users-service';
+
+// export default function NavBar() {
+//   const isLoggedIn = !!getUser();
+//   console.log("isLoggedIn:", isLoggedIn); // Log isLoggedIn
+
+//   return (
+//     <nav className="navbar">
+//       <div>
+//         <NavLink to="/">Recipes</NavLink> &nbsp;
+//         {isLoggedIn ? (
+//           <NavLink to="/profile">Profile</NavLink>
+//         ) : (
+//           <NavLink to="/login">Login</NavLink>
+//         )}
+//         &nbsp;
+//       </div>
+//     </nav>
+//   );
+// }
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 import '../App.css'; 
-import { getUser } from '../utilities/users-service';
 
-export default function NavBar() {
-  const isLoggedIn = !!getUser();
-  console.log("isLoggedIn:", isLoggedIn); // Log isLoggedIn
-
+export default function NavBar({ isLoggedIn, onLogout }) {
   return (
     <nav className="navbar">
       <div>
         <NavLink to="/">Recipes</NavLink> &nbsp;
         {isLoggedIn ? (
-          <NavLink to="/profile">Profile</NavLink>
+          <>
+            <NavLink to="/profile">Profile</NavLink>
+            <button onClick={onLogout}>Logout</button> {/* Add logout button */}
+          </>
         ) : (
           <NavLink to="/login">Login</NavLink>
         )}

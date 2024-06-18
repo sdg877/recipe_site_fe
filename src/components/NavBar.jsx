b6f2916194faf6e3a9d0a81cd1fb6a0ad5905dd1
Sdg877 <sdrakegill@gmail.com>
2024-06-18 13:52:21 +0100

    trouble shooting navbar issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,35 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import '../App.css'; 
+// import { getUser } from '../utilities/users-service'
+
+// export default function NavBar() {
+//   const isLoggedIn = !!getUser();
+
+//   return (
+//     <nav className="navbar">
+//       <div>
+//         <NavLink to="/">Recipes</NavLink> &nbsp;
+//         {isLoggedIn ? (
+//           <NavLink to="/profile">Profile</NavLink>
+//         ) : (
+//           <NavLink to="/login">Login</NavLink>
+//         )}
+//         &nbsp;
+//       </div>
+//     </nav>
+//   );
+// }
+
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 import '../App.css'; 
-import { getUser } from '../utilities/users-service'
+import { getUser } from '../utilities/users-service';
 
 export default function NavBar() {
   const isLoggedIn = !!getUser();
+  console.log("isLoggedIn:", isLoggedIn); // Log isLoggedIn
 
   return (
     <nav className="navbar">
@@ -20,5 +45,3 @@ export default function NavBar() {
     </nav>
   );
 }
-
-

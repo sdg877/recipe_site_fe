15f01a8e3e067178cca180b48ea5afea62af1e91
Sdg877 <sdrakegill@gmail.com>
2024-06-19 13:15:40 +0100

    resolving bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import LoginPage from './pages/LoginPage.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import HomePage from '../src/pages/HomePage.jsx';
-import NavBar from './components/NavBar.jsx';
-import RecipeView from './pages/RecipeView.jsx';
-
-function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <NavBar />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/recipes/:id" element={<RecipeView />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-
-
 // import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import './App.css';
 // import LoginPage from './pages/LoginPage.jsx';
 // import ProfilePage from './pages/ProfilePage.jsx';
@@ -37,19 +9,47 @@ export default App;
 
 // function App() {
 //   return (
-//     <div className='App'>
-//       <NavBar /> 
-//       <Routes>
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/profile" element={<ProfilePage />} />
-//         <Route path="/recipes/:id" element={<RecipeView />} />
-//         <Route path="/" element={<HomePage />} />
-//       </Routes>
-//     </div>
+//     <Router>
+//       <div className='App'>
+//         <NavBar />
+//         <Routes>
+//           <Route path="/login" element={<LoginPage />} />
+//           <Route path="/profile" element={<ProfilePage />} />
+//           <Route path="/recipes/:id" element={<RecipeView />} />
+//           <Route path="/" element={<HomePage />} />
+//         </Routes>
+//       </div>
+//     </Router>
 //   );
 // }
 
 // export default App;
+
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/LoginPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import HomePage from '../src/pages/HomePage.jsx';
+import NavBar from './components/NavBar.jsx';
+import RecipeView from './pages/RecipeView.jsx';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar /> 
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/recipes/:id" element={<RecipeView />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
 
 
 // import React, { useState } from 'react';

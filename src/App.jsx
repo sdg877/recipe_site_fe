16da71a6f95710d57e430eb4a9b135b9364a201a
Sdg877 <sdrakegill@gmail.com>
2024-06-19 13:07:13 +0100

    fixing issue with broken links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
@@ -9,19 +9,47 @@ import RecipeView from './pages/RecipeView.jsx';
 
 function App() {
   return (
-    <div className='App'>
-      <NavBar /> 
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/recipes/:id" element={<RecipeView />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className='App'>
+        <NavBar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/recipes/:id" element={<RecipeView />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import LoginPage from './pages/LoginPage.jsx';
+// import ProfilePage from './pages/ProfilePage.jsx';
+// import HomePage from '../src/pages/HomePage.jsx';
+// import NavBar from './components/NavBar.jsx';
+// import RecipeView from './pages/RecipeView.jsx';
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <NavBar /> 
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/profile" element={<ProfilePage />} />
+//         <Route path="/recipes/:id" element={<RecipeView />} />
+//         <Route path="/" element={<HomePage />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 // import React, { useState } from 'react';

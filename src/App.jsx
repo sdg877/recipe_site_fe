5b2c10da545e79a7ce49931cdadf1c9ef6779df7
Sdg877 <sdrakegill@gmail.com>
2024-06-19 12:54:08 +0100

    amending app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/LoginPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import HomePage from '../src/pages/HomePage.jsx';
+import NavBar from './components/NavBar.jsx';
+import RecipeView from './pages/RecipeView.jsx';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar /> 
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/recipes/:id" element={<RecipeView />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+
 // import React, { useState } from 'react';
 // import { Routes, Route } from 'react-router-dom';
 // import './App.css';
@@ -148,39 +174,39 @@
 
 // export default App;
 
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import LoginPage from './pages/LoginPage.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import HomePage from '../src/pages/HomePage.jsx';
-import NavBar from './components/NavBar.jsx';
-import RecipeView from './pages/RecipeView.jsx';
-import { getUser, login } from './utilities/users-service'; // import getUser and login
+// import React, { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import LoginPage from './pages/LoginPage.jsx';
+// import ProfilePage from './pages/ProfilePage.jsx';
+// import HomePage from '../src/pages/HomePage.jsx';
+// import NavBar from './components/NavBar.jsx';
+// import RecipeView from './pages/RecipeView.jsx';
+// import { getUser, login } from './utilities/users-service'; // import getUser and login
 
-function App() {
-  // Manage authentication state
-  const [isLoggedIn, setIsLoggedIn] = useState(!!getUser());
+// function App() {
+//   // Manage authentication state
+//   const [isLoggedIn, setIsLoggedIn] = useState(!!getUser());
 
-  // Function to handle login
-  const handleLogin = () => {
-    setIsLoggedIn(true); // Update isLoggedIn state
-  };
+//   // Function to handle login
+//   const handleLogin = () => {
+//     setIsLoggedIn(true); // Update isLoggedIn state
+//   };
 
-  return (
-    <div className='App'>
-      {/* Pass isLoggedIn and handleLogin to NavBar */}
-      <NavBar isLoggedIn={isLoggedIn} />
-      <Routes>
-        {/* Pass handleLogin to LoginPage */}
-        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/recipes/:id" element={<RecipeView />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
-  );
-}
+//   return (
+//     <div className='App'>
+//       {/* Pass isLoggedIn and handleLogin to NavBar */}
+//       <NavBar isLoggedIn={isLoggedIn} />
+//       <Routes>
+//         {/* Pass handleLogin to LoginPage */}
+//         <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+//         <Route path="/profile" element={<ProfilePage />} />
+//         <Route path="/recipes/:id" element={<RecipeView />} />
+//         <Route path="/" element={<HomePage />} />
+//       </Routes>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 

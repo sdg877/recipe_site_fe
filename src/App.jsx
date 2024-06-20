be3cb75655952f871ee3294eeddb277bf4cf339f
Sdg877 <sdrakegill@gmail.com>
2024-06-20 12:48:03 +0100

    removing unused code and final styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -22,31 +22,3 @@ function App() {
 }
 
 export default App;
-
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './App.css';
-// import LoginPage from './pages/LoginPage';
-// import ProfilePage from './pages/ProfilePage';
-// import HomePage from './pages/HomePage';
-// import NavBar from './components/NavBar';
-// import RecipeView from './pages/RecipeView';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className='App'>
-//         <NavBar />
-//         <Routes>
-//           <Route path="/login" element={<LoginPage />} />
-//           <Route path="/profile" element={<ProfilePage />} />
-//           <Route path="/recipes/:id" element={<RecipeView />} />
-//           <Route path="/" element={<HomePage />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
